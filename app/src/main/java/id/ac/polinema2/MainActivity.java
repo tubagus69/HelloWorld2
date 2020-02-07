9693f48bcfed9525750d8a0b73ece4e8dee93c6a
tubagus69 <tubagusputra6969@gmail.com>
2020-02-07 09:01:35 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/polinema2/MainActivity.java
@@ -0,0 +1,14 @@
+package id.ac.polinema2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
